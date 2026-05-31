fcdf9e8fdfaf3884f07970a9bed3a94eeacdeee9
Alejandro Chen Santoro <alejandrocsdev@gmail.com>
2026-06-01 00:42:07 +0800

    comment out templates api

--- src/controllers/template.controller.ts
+++ src/controllers/template.controller.ts
@@ -1,34 +1,34 @@
-import { asyncHandler } from '../middlewares/index.js';
-import { templateService } from '../services/template.service.js';
+// import { asyncHandler } from '../middlewares/index.js';
+// import { templateService } from '../services/template.service.js';
 
-export class TemplateController {
-  public readonly getTemplates = asyncHandler(async (req, res) => {
-    const userId = Number(req.params.userId);
-    const data = await templateService.getTemplates(userId);
-    res.json(data);
-  });
+// export class TemplateController {
+//   public readonly getTemplates = asyncHandler(async (req, res) => {
+//     const userId = Number(req.params.userId);
+//     const data = await templateService.getTemplates(userId);
+//     res.json(data);
+//   });
 
-  public readonly createTemplate = asyncHandler(async (_req, res) => {
-    const data = await templateService.createTemplate();
-    res.json(data);
-  });
+//   public readonly createTemplate = asyncHandler(async (_req, res) => {
+//     const data = await templateService.createTemplate();
+//     res.json(data);
+//   });
 
-  public readonly editTemplate = asyncHandler(async (req, res) => {
-    const { userId, templateId, templateHash, volumeSize } = req.body as {
-      userId: number;
-      templateId: number;
-      templateHash: string;
-      volumeSize: number;
-    };
-    const data = await templateService.editTemplate(userId, templateId, templateHash, volumeSize);
-    res.json(data);
-  });
+//   public readonly editTemplate = asyncHandler(async (req, res) => {
+//     const { userId, templateId, templateHash, volumeSize } = req.body as {
+//       userId: number;
+//       templateId: number;
+//       templateHash: string;
+//       volumeSize: number;
+//     };
+//     const data = await templateService.editTemplate(userId, templateId, templateHash, volumeSize);
+//     res.json(data);
+//   });
 
-  public readonly deleteTemplate = asyncHandler(async (req, res) => {
-    const templateId = Number(req.params.templateId);
-    const data = await templateService.deleteTemplate(templateId);
-    res.json(data);
-  });
-}
+//   public readonly deleteTemplate = asyncHandler(async (req, res) => {
+//     const templateId = Number(req.params.templateId);
+//     const data = await templateService.deleteTemplate(templateId);
+//     res.json(data);
+//   });
+// }
 
-export const templateController = new TemplateController();
+// export const templateController = new TemplateController();
--- src/routes/template.routes.ts
+++ src/routes/template.routes.ts
@@ -1,11 +1,11 @@
-import { Router } from 'express';
-import { templateController } from '../controllers/template.controller.js';
+// import { Router } from 'express';
+// import { templateController } from '../controllers/template.controller.js';
 
-const router = Router();
+// const router = Router();
 
-router.get('/:userId', templateController.getTemplates);
-router.post('/', templateController.createTemplate);
-router.put('/', templateController.editTemplate);
-router.delete('/:templateId', templateController.deleteTemplate);
+// router.get('/:userId', templateController.getTemplates);
+// router.post('/', templateController.createTemplate);
+// router.put('/', templateController.editTemplate);
+// router.delete('/:templateId', templateController.deleteTemplate);
 
-export const templateRoutes = router;
+// export const templateRoutes = router;
